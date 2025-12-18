baa80ace46ca5261dbac9f04a29454714f980a5f
vv. <102243593+vvrednaya@users.noreply.github.com>
2025-12-18 15:15:20 +0300

    Add files via upload

---
+++ bootstrap/main.js
@@ -0,0 +1,46 @@
+function stop(e){
+    e.stopPropagation()
+    e.preventDefault()
+  }
+
+window.onload = function() {
+
+    // оверлей авторизации
+    const registraciya = $("#registraciya");
+    const login = $("#login");
+		registraciya.on('click', (event) => stop(event))
+		login.on('click', (event) => stop(event))
+
+    $("#openLogin").on("click", (event) => {
+        stop(event);
+        login.show()
+        registraciya.hide()
+    })
+
+    $("#openReg").on("click", (event) => {
+        stop(event);
+        registraciya.show()
+        login.hide()
+    })
+
+    const overlay = $("#overlay");
+
+    const elementsIdToOpen = ['openoverlay']
+    const elementsIdToClose = ['overlay', 'voiti', 'zareg']
+
+    elementsIdToOpen.forEach(element => {
+        $(`#${element}`).on('click', (event) => {
+            stop(event);
+            overlay.show()
+        })
+    })
+
+    elementsIdToClose.forEach(element => {
+        $(`#${element}`).on('click', (event) => {
+            stop(event);
+            overlay.hide()
+        })
+    })
+
+}
+    
